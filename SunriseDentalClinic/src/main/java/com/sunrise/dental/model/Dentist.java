4779a3c71502a8483f6d9229a7bbd0e12e44a718
Dinuka Chamod <dinukachamod03@gmail.com>
2026-09-02 09:32:05 +0530

    id

--- SunriseDentalClinic/src/main/java/com/sunrise/dental/model/Dentist.java
+++ SunriseDentalClinic/src/main/java/com/sunrise/dental/model/Dentist.java
@@ -12,10 +12,11 @@ public class Dentist {
         this.name = name;
         this.specialization = specialization;
     }
-
+//id
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
+    
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
